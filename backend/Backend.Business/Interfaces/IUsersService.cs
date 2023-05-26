5b3527c20d4559f0f1d247192510dc4772ae1862
DofSekai <catez.jeremy@hotmail.com>
2023-05-26 11:25:49 +0200

    bugfix

--- backend/Backend.Business/Interfaces/IUsersService.cs
+++ backend/Backend.Business/Interfaces/IUsersService.cs
@@ -3,7 +3,7 @@ using Backend.Common.DTO;
 namespace Backend.Business.Interfaces
 {
     public interface IUsersService {
-        Task<IEnumerable<User>> GetAllGames(CancellationToken cancellationToken);
+        Task<IEnumerable<User>> GetAllUsers(CancellationToken cancellationToken);
         Task<User?> GetById(int id);
         Task<IEnumerable<User>> SearchByName(string name);
         Task Create(User user);
