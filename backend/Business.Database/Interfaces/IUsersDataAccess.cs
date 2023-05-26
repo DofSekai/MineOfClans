88186e41394b0fb6560ac6ea1981d77786ad0c10
DofSekai <catez.jeremy@hotmail.com>
2023-05-26 11:30:36 +0200

    bugfix

--- backend/Business.Database/Interfaces/IUsersDataAccess.cs
+++ backend/Business.Database/Interfaces/IUsersDataAccess.cs
@@ -2,7 +2,7 @@ using Backend.Common.DAO;
 
 namespace Business.Database.Interfaces {
     public interface IUsersDataAccess {
-        IAsyncEnumerable<User> GetAllUser();
+        IAsyncEnumerable<User> GetAllUsers();
         Task<User?> GetById(int id);
         Task<IEnumerable<User>> SearchByName(string name);
         Task Create(User user);
