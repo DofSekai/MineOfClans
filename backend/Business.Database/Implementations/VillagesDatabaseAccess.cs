beec136c1efa03532bfe645ef653f257a9fb71f8
DofSekai <catez.jeremy@hotmail.com>
2023-05-26 16:55:53 +0200

    interface villages database access

---
+++ backend/Business.Database/Implementations/VillagesDatabaseAccess.cs
@@ -0,0 +1,26 @@
+using Backend.Common.DAO;
+using Business.Database.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Business.Database.Implementations {
+    public class VillagesDatabaseAccess : IVillagesDataAccess {
+        private readonly DatabaseContext _databaseContext;
+        
+        public VillagesDatabaseAccess(DatabaseContext databaseContext) {
+            _databaseContext = databaseContext;
+        }
+        
+        public IAsyncEnumerable<Village> GetAllVillages() {
+            return _databaseContext.villages.AsAsyncEnumerable();
+        }
+
+        public async Task<Village?> GetById(int id) {
+            return await _databaseContext.villages.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task Create(Village village) {
+            _databaseContext.village.Add(village);
+            await _databaseContext.SaveChangesAsync();
+        }
+    }
+}
