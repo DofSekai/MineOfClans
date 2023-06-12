1be05094f7fa809a66e9f0ca6c213cdc6c5923fd
lylianhvt <lylianhvt@gmail.com>
2023-06-12 14:12:50 +0200

    temp

--- frontend/src/business/createUser.js
+++ frontend/src/business/createUser.js
@@ -1,6 +1,6 @@
 
 export default async function createUser(user) {
-    const res = await fetch('https://localhost:7227/api/Users', {
+    const res = await fetch('http://localhost:5235/api/Users', {
         method: 'post',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(user)
