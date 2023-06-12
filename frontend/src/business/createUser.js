1214a5f5a151caa6b63afc04ffc74d1e82b082a6
DofSekai <catez.jeremy@hotmail.com>
2023-06-12 15:17:41 +0200

    add config file frontend

--- frontend/src/business/createUser.js
+++ frontend/src/business/createUser.js
@@ -1,6 +1,7 @@
+import config from '../config.js';
 
 export default async function createUser(user) {
-    const res = await fetch('http://localhost:5235/api/Users', {
+    const res = await fetch('http://localhost:'+config.SWAGGER_PORT+'/api/Users', {
         method: 'post',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(user)
