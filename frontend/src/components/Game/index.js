e3ded97769cf89aa03ed13bab5578b9d8f85bdd9
lylianhvt <lylianhvt@gmail.com>
2023-06-12 13:02:49 +0200

    game page v1.1

--- frontend/src/components/Game/index.js
+++ frontend/src/components/Game/index.js
@@ -1,4 +1,5 @@
 import "../App/default.css"
+import "./test.css"
 import mine from "../../img/mine.png"
 import logo from "../../img/logo.png"
 import golem from "../../img/golem.png"
@@ -17,7 +18,7 @@ return(
             <br></br>
             
             <div class="flex items-center justify-center">
-              <img src={logo} alt="Logo" class="w-28 h-28 rounded-full mr-2"></img>
+              <img src={logo} alt="Logo" class="test"></img>
             <h1 class="font-bold text-3xl">MineOfClans</h1>         
             </div>
             <br></br>
@@ -42,20 +43,22 @@ return(
     <div class="w-1/2 h-full border-2 border-black p-4">
       <h1 class="text-center text-2xl font-bold p-4">Village</h1>
       <br></br>
-      <div class="flex h-screen">
-        <div class="border-4 border-black w-1/2 h-2/3 p-4">
+      <div class="flex h-1/2">
+        <div class="border-4 border-black w-1/2 h-72 p-4">
             <p>Niveau Mine : X</p>
             <br></br>
             <img src={mine} alt="mine" class="w-36 h-36 rounded-3xl"></img>
             <br></br>
             <button class="bg-lime-500 text-white rounded-xl p-1">Upgrade</button>
         </div>
-        <div class="border-4 border-black w-1/2 h-2/3 p-4">
+        <div class="border-4 border-black w-1/2 h-72 p-4">
             <p>azerty</p>
         </div>
-
+        
       </div>
-      
+      <div class="border-4 border-black h-72 p-4">
+            <p>dofsekai</p>
+        </div>
     </div>
     <div class="w-1/4 h- border-2 border-black p-4">
       <h1 class="text-center text-2xl font-bold p-4">Défense</h1>
