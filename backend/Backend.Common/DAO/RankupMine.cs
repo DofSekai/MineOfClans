028df0fee576d2a65982cb8225a75ba4dda06538
DofSekai <catez.jeremy@hotmail.com>
2023-06-16 00:15:35 +0200

    rankup mine common

---
+++ backend/Backend.Common/DAO/RankupMine.cs
@@ -0,0 +1,8 @@
+﻿namespace Backend.Common.DAO {
+    public class RankupMine {
+        public int Id { get; set; }
+        public int Irons { get; set; }
+        public int Diamonds { get; set; }
+        public int Emeralds { get; set; }
+    }
+}
