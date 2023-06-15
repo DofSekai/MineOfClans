6814d2017d8d4fcc6b0c4fe4e3d1c49673e9a68b
DofSekai <catez.jeremy@hotmail.com>
2023-06-15 14:57:03 +0200

    MaxItems

--- backend/Backend.Common/DAO/Village.cs
+++ backend/Backend.Common/DAO/Village.cs
@@ -6,9 +6,11 @@ namespace Backend.Common.DAO {
         public int Emeralds { get; set; } = 10;
         public int Walls { get; set; }
         public int Golems { get; set; }
+        public int Towers { get; set; }
         public LevelMine LevelMine { get; set; } 
         public int LevelMineId { get; set; }
-        public int LevelHDV { get; set; } = 1;
+        public MaxItems LevelHDV { get; set; }
+        public int LevelHDV_Id { get; set; }
         public int LastUpdate { get; set; } = (int) (DateTime.Now - new DateTime(1970, 1, 1, 0, 0, 0)).TotalSeconds;
     }
 }
---
+++ backend/Backend.Database/Migrations/20230615124143_InsertMaxItems.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Business.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class InsertMaxItems : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO \"maxItems\"(\"MaxGolems\", \"MaxWalls\", \"MaxTowers\") VALUES(2, 20, 0)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
