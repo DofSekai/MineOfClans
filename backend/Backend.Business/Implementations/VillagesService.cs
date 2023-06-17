9787f22bff960bb9225a9474712e16088879e428
DofSekai <catez.jeremy@hotmail.com>
2023-06-17 11:26:30 +0200

    Upgrade Mine

--- backend/Backend.Business/Implementations/VillagesService.cs
+++ backend/Backend.Business/Implementations/VillagesService.cs
@@ -1,15 +1,21 @@
 using Backend.Business.Interfaces;
 using Backend.Common.DTO;
+using Backend.Database.Interfaces;
 using Business.Database.Interfaces;
 using Microsoft.Extensions.Logging;
+using LevelMine = Backend.Common.DAO.LevelMine;
 
 namespace Backend.Business.Implementations {
     public class VillagesService : IVillagesService {
         private readonly IVillagesDataAccess _villagesDataAccess;
+        private readonly ILevelMinesDataAccess _levelMinesDataAccess;
+        private readonly IRankupMinesDataAccess _rankupMinesDataAccess;
         private readonly ILogger<VillagesService> _logger;
         
-        public VillagesService(IVillagesDataAccess villagesDataAccess, ILogger<VillagesService> logger) {
+        public VillagesService(IVillagesDataAccess villagesDataAccess, IRankupMinesDataAccess rankupMinesDataAccess, ILevelMinesDataAccess levelMinesDataAccess, ILogger<VillagesService> logger) {
             _villagesDataAccess = villagesDataAccess;
+            _rankupMinesDataAccess = rankupMinesDataAccess;
+            _levelMinesDataAccess = levelMinesDataAccess;
             _logger = logger;
         }
         
@@ -59,13 +65,15 @@ namespace Backend.Business.Implementations {
             int LastUpdate = village.LastUpdate;
             int NewLastUpdate = (int) (DateTime.Now - new DateTime(1970, 1, 1, 0, 0, 0)).TotalSeconds;
             int Multiplicator = NewLastUpdate - LastUpdate;
-            int IronRate = village.LevelMine.IronRate;
-            int DiamondRate = village.LevelMine.DiamondRate;
-            int EmeraldRate = village.LevelMine.EmeraldRate;
-            int IronMaxRate = village.LevelMine.IronMaxRate;
-            int DiamondMaxRate = village.LevelMine.DiamondMaxRate;
-            int EmeraldMaxRate = village.LevelMine.EmeraldMaxRate;
+            LevelMine LevelMine = village.LevelMine;
+            int IronRate = LevelMine.IronRate;
+            int DiamondRate = LevelMine.DiamondRate;
+            int EmeraldRate = LevelMine.EmeraldRate;
+            int IronMaxRate = LevelMine.IronMaxRate;
+            int DiamondMaxRate = LevelMine.DiamondMaxRate;
+            int EmeraldMaxRate = LevelMine.EmeraldMaxRate;
             
+            // Update Resources
             if (village.Irons + Multiplicator * IronRate < IronMaxRate) { 
                 village.Irons += Multiplicator * IronRate;
             }
@@ -88,6 +96,24 @@ namespace Backend.Business.Implementations {
             else
             {
                 village.Emeralds = EmeraldMaxRate;
+            }
+
+            var RankupMine = await _rankupMinesDataAccess.GetById(LevelMine.Id);
+            if (RankupMine != null)
+            {
+                if (village.Irons >= RankupMine.Irons && village.Diamonds >= RankupMine.Diamonds &&
+                    village.Emeralds >= RankupMine.Emeralds)
+                {
+                    LevelMine NextLevelMine = await _levelMinesDataAccess.GetById(village.LevelMineId + 1);
+
+                    if (NextLevelMine != null)
+                    {
+                        village.Irons -= RankupMine.Irons;
+                        village.Diamonds -= RankupMine.Diamonds;
+                        village.Emeralds -= RankupMine.Emeralds;
+                        village.LevelMine = NextLevelMine;
+                    }
+                }
             }
 
             village.LastUpdate = NewLastUpdate;
