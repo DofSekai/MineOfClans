fd58eb14f26938ad8b4a919538ded92938bbbbab
MissWeasley <fasquel.maud@gmail.com>
2023-07-04 18:20:57 +0200

    add village

--- frontend/src/components/City/index.js
+++ frontend/src/components/City/index.js
@@ -3,6 +3,7 @@ import { useLocation } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import getCity from '../../business/getCity.js'; 
 import logo from "../../img/logo.png"
+import village from "../../img/village.png"
 import axios from "axios"
 import config from '../../config.js';
 
@@ -15,7 +16,9 @@ export default function City(){
     useEffect(() => {
         const searchParams = new URLSearchParams(location.search); 
         setName(searchParams.get('name'));
+        
     }, [location.search]);
+   console.log("ok"+name);
    
     useEffect(() => {
         axios.get(`http://localhost:${config.SWAGGER_PORT}/api/Users/search/${name}`)
@@ -53,6 +56,7 @@ export default function City(){
     <hr></hr>
     <br></br>
     <Link to="/login"><button id="btn_deco">Déconnexion</button> </Link>
+    <Link to={`/registercity?name=${name}`}><button id="btn_deco">Add village</button> </Link>
     <div class="flex items-center justify-center">
         <img src={logo} alt="Logo" class="test"></img>
         <h1 class="font-bold text-3xl">MineOfClans</h1>         
@@ -61,13 +65,19 @@ export default function City(){
     <h1 class="font-bold text-2xl">Villages de {name} !</h1>   
     <br></br>
     <hr></hr>
-    {citys.map(city => (
+    <br></br>
+    <div class=" bg-slate-100 rounded-full py-9 flex flex-col justify-center sm:py-12">
+      <div class="grid grid-cols-3 gap-4">      
+        {citys.map(city => (
             <div class="flex flex-col items-center">
-            <a href={`http://localhost:3000/game?name=${name}&village=${city.name}`}> 
-            <h3 class="text-center mt-2">{city.name}</h3></a>
-        </div>
+              <a href={`http://localhost:3000/game?name=${name}&village=${city.name}`}><img src={village} className="w-50 h-40" alt="img"/> 
+              <h2 class="text-center mt-2 font-extrabold text-xl">{city.name}</h2></a>
+          </div>
         
-    ))}
+        ))}
+      </div>
+    </div>
+    
 
 
   </section>   
