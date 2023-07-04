a25dcbf195fd6a59a518c022b5dbd71d8d0e7c0c
DofSekai <catez.jeremy@hotmail.com>
2023-07-04 17:50:42 +0200

    AddLevelHdvToVillage

--- backend/Backend.Common/DAO/Village.cs
+++ backend/Backend.Common/DAO/Village.cs
@@ -14,6 +14,9 @@
         public int Towers { get; set; }
         public int LevelMineId { get; set; } = 1;
         public LevelMine LevelMine { get; set; }
+        public int LevelHdvId { get; set; } = 1;
+        
+        public LevelHdv LevelHdv { get; set; }
         public int LastUpdate { get; set; } = (int) (DateTime.Now - new DateTime(1970, 1, 1, 0, 0, 0)).TotalSeconds;
     }
 }
--- backend/Backend.Common/DTO/Village.cs
+++ backend/Backend.Common/DTO/Village.cs
@@ -15,8 +15,13 @@ public class Village
     public int Golems { get; set; }
     public int Walls { get; set; }
     public int Towers { get; set; }
-    [JsonIgnore] public int LevelMineId { get; set; } = 1;
+    [JsonIgnore] 
+    public int LevelMineId { get; set; } = 1;
     public LevelMine LevelMine { get; set; }
+    [JsonIgnore]
+    public int LevelHdvId { get; set; } = 1;
+        
+    public LevelHdv LevelHdv { get; set; }
     public int LastUpdate { get; set; } = (int) (DateTime.Now - new DateTime(1970, 1, 1, 0, 0, 0)).TotalSeconds;
 }
     
